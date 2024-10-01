dde654a3e87347e3a1e86c0e5548a8080249068b
Issac Cabral <clidenorissac@gmail.com>
2024-10-01 13:27:36 -0300

    ✨ feat: add skeleton component

--- src/lib/axios.tsx
+++ src/lib/axios.tsx
@@ -5,3 +5,13 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true, // envia automaticamente os cookies do front-end para o back-end
 });
+
+// antes de todas as requisições do axios, essa função é executada,
+// caso a variável de ambiente abaixo seja true
+if (env.VITE_API_ENABLE_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    return config;
+  });
+}
