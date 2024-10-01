9abb9ba81a399e35cd578195cd5a611fb1c21b09
Issac Cabral <clidenorissac@gmail.com>
2024-10-01 11:50:49 -0300

    ✨ feat: get managed restaurant

--- src/lib/axios.tsx
+++ src/lib/axios.tsx
@@ -3,4 +3,5 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+  withCredentials: true, // envia automaticamente os cookies do front-end para o back-end
 });
